ece59a3995da439ec8186a26e27e05c76d592e21
David Grant <davidttgg@gmail.com>
2022-04-28 09:53:17 +0100

    Revamp trajectory e zero sep.

--- harmonica/orbit/trajectories.cpp
+++ harmonica/orbit/trajectories.cpp
@@ -29,63 +29,31 @@ void orbital_trajectories(double t0, double period, double a,
     auto nus_grad_ = nus_grad.mutable_unchecked<2>();
   }
 
-  // Iterate evaluation times.
-  for (py::ssize_t i = 0; i < times_.shape(0); i++) {
+  const double n = fractions::twopi / period;
+  const double cos_i = std::cos(inc);
 
-      double x, y, psi;
-      const double n = fractions::twopi / period;
-      const double cos_i = std::cos(inc);
+  if (ecc == 0.) {
 
-      if (ecc == 0.) {
+    // Iterate evaluation times.
+    for (py::ssize_t i = 0; i < times_.shape(0); i++) {
 
-        // Compute time of periastron.
-        double tp = t0 - fractions::pi_d_2 / n;
+      // Compute time of periastron.
+      double tp = t0 - fractions::pi_d_2 / n;
 
-        // Compute mean anomaly.
-        double M = (times_(i) - tp) * n;
+      // Compute mean anomaly.
+      double M = (times_(i) - tp) * n;
 
-        // Compute sine and cosine of the true anomaly.
-        std::tuple<double, double> sin_cos_f = std::make_tuple(
-            std::sin(M), std::cos(M));
+      // Compute sine and cosine of the true anomaly.
+      std::tuple<double, double> sin_cos_f = std::make_tuple(
+          std::sin(M), std::cos(M));
 
-        // Compute location of planet centre relative to stellar centre.
-        x = a * std::get<1>(sin_cos_f);
-        y = a * cos_i * std::get<0>(sin_cos_f);
+      // Compute location of planet centre relative to stellar centre.
+      double x = a * std::get<1>(sin_cos_f);
+      double y = a * cos_i * std::get<0>(sin_cos_f);
 
-        // Compute angle between x-axis and planet velocity.
-        psi = cos_i * std::atan(-std::get<1>(sin_cos_f)
-                                / std::get<0>(sin_cos_f));
-
-      } else {
-
-        // Compute time of periastron.
-        const double n = fractions::twopi / period;
-        const double sin_w = std::sin(omega);
-        const double cos_w = std::cos(omega);
-        const double cos_i = std::cos(inc);
-        double E0 = 2. * std::atan2(std::sqrt(1. - ecc) * cos_w,
-                                    std::sqrt(1. + ecc) * (1 + sin_w));
-        double M0 = E0 - ecc * std::sin(E0);
-        double tp = t0 - M0 / n;
-
-        // Compute mean anomaly.
-        double M = (times_(i) - tp) * n;
-
-        // Compute sine and cosine of the true anomaly.
-        std::tuple<double, double> sin_cos_f = solve_kepler(M, ecc);
-
-        // Compute location of planet centre relative to stellar centre.
-        double r = a * (1 - ecc * ecc) / (1 + ecc * std::get<1>(sin_cos_f));
-        const double sin_fpw = std::get<1>(sin_cos_f) * sin_w
-                               + std::get<0>(sin_cos_f) * cos_w;
-        const double cos_fpw = std::get<1>(sin_cos_f) * cos_w
-                               - std::get<0>(sin_cos_f) * sin_w;
-        x = r * cos_fpw;
-        y = r * cos_i * sin_fpw;
-
-        // Compute angle between x-axis and planet velocity.
-        psi = cos_i * std::atan(-cos_fpw / sin_fpw);
-      }
+      // Compute angle between x-axis and planet velocity.
+      double psi = cos_i * std::atan(-std::get<1>(sin_cos_f)
+                                     / std::get<0>(sin_cos_f));
 
       // Compute separation distance between planet and stellar centres.
       ds_(i) = std::sqrt(x * x + y * y);
@@ -95,5 +63,48 @@ void orbital_trajectories(double t0, double period, double a,
 
       // Optionally compute derivatives.
 
+    }
+
+  } else {
+
+    const double sin_w = std::sin(omega);
+    const double cos_w = std::cos(omega);
+
+    // Iterate evaluation times.
+    for (py::ssize_t i = 0; i < times_.shape(0); i++) {
+
+      // Compute time of periastron.
+      double E0 = 2. * std::atan2(std::sqrt(1. - ecc) * cos_w,
+                                  std::sqrt(1. + ecc) * (1 + sin_w));
+      double M0 = E0 - ecc * std::sin(E0);
+      double tp = t0 - M0 / n;
+
+      // Compute mean anomaly.
+      double M = (times_(i) - tp) * n;
+
+      // Compute sine and cosine of the true anomaly.
+      std::tuple<double, double> sin_cos_f = solve_kepler(M, ecc);
+
+      // Compute location of planet centre relative to stellar centre.
+      double r = a * (1 - ecc * ecc) / (1 + ecc * std::get<1>(sin_cos_f));
+      const double sin_fpw = std::get<1>(sin_cos_f) * sin_w
+                             + std::get<0>(sin_cos_f) * cos_w;
+      const double cos_fpw = std::get<1>(sin_cos_f) * cos_w
+                             - std::get<0>(sin_cos_f) * sin_w;
+      double x = r * cos_fpw;
+      double y = r * cos_i * sin_fpw;
+
+      // Compute angle between x-axis and planet velocity.
+      double psi = cos_i * std::atan(-cos_fpw / sin_fpw);
+
+      // Compute separation distance between planet and stellar centres.
+      ds_(i) = std::sqrt(x * x + y * y);
+
+      // Compute angle between planet velocity and stellar centre.
+      nus_(i) = std::atan2(y, x) - psi;
+
+      // Optionally compute derivatives.
+
+    }
   }
 }
--- tests/test_grad_df_de.py
+++ tests/test_grad_df_de.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from harmonica import bindings
+
 
 def M0_func(e, w):
     alpha = ((1 - e) ** 0.5 * np.cos(w)) / ((1 + e) ** 0.5 * (1 + np.sin(w)))
@@ -15,6 +17,12 @@ def M_func(e, w, p, t0, t):
     tp = t0 - M0 / n
     M = (t - tp) * n
     return M
+
+
+def sinf_func(e, w, p, t0, t):
+    M = M_func(e, w, p, t0, t)
+    sinf, cosf = bindings.test_solve_kepler(M, e)
+    return sinf
 
 
 def dE0_de_total(e, w):
@@ -37,6 +45,26 @@ def dM_de_total(e, w):
     return dM_dE0_partial * dE0_de_total(e, w) + dM_de_partial * de_de_total
 
 
+def df_de_total(e, w, p, t0, t):
+    M = M_func(e, w, p, t0, t)
+    sinf, cosf = bindings.test_solve_kepler(M, e)
+
+    df_dM_partial = (1 + e * cosf)**2 / (1 - e**2)**1.5
+    df_de_partial = ((2 + e * cosf) * sinf) / (1 - e**2)
+    de_de_total = 1
+
+    return df_dM_partial * dM_de_total(e, w) + df_de_partial * de_de_total
+
+
+def dsinf_de_total(e, w, p, t0, t):
+    M = M_func(e, w, p, t0, t)
+    sinf, cosf = bindings.test_solve_kepler(M, e)
+
+    dsinf_df_total = cosf
+
+    return dsinf_df_total * df_de_total(e, w, p, t0, t)
+
+
 def grad_arrow(x_draw, x, y, grad):
     c = y - grad * x
     return grad * x_draw + c
@@ -48,24 +76,24 @@ while True:
     p_a = np.random.uniform(0, 10)
     t0_a = np.random.uniform(0, 10)
     t_a = np.random.uniform(0, 10)
-    M_a = M_func(ecc_a, w_a, p_a, t0_a, t_a)
+    sinf_a = sinf_func(ecc_a, w_a, p_a, t0_a, t_a)
 
     epsilon = 1e-6
     ecc_b = ecc_a + epsilon
-    M_b = M_func(ecc_b, w_a, p_a, t0_a, t_a)
+    sinf_b = sinf_func(ecc_b, w_a, p_a, t0_a, t_a)
 
-    M_a_grad = dM_de_total(ecc_a, w_a)
+    sinf_a_grad = dsinf_de_total(ecc_a, w_a, p_a, t0_a, t_a)
 
-    plt.scatter(ecc_a, M_a, 
+    plt.scatter(ecc_a, sinf_a,
                 label='$e_1$')
-    plt.scatter(ecc_b, M_b, 
+    plt.scatter(ecc_b, sinf_b,
                 label='$e_1 + \\epsilon$: $\\epsilon={}$'.format(epsilon))
     x_arrow = np.linspace(ecc_a, ecc_b, 2)
-    plt.plot(x_arrow, grad_arrow(x_arrow, ecc_a, M_a, M_a_grad),
-             label='Gradient: $\\frac{dM}{de}$')
+    plt.plot(x_arrow, grad_arrow(x_arrow, ecc_a, sinf_a, sinf_a_grad),
+             label='Gradient: $\\frac{d \sin{f}}{de}$')
 
     plt.legend()
     plt.xlabel('e')
-    plt.ylabel('M')
+    plt.ylabel('$\sin{f}$')
     plt.tight_layout()
     plt.show()
