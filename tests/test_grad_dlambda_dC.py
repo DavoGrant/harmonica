db3f92535d39327e0316f7de2f73bd249610774a
David Grant <davidttgg@gmail.com>
2022-04-29 18:19:58 +0100

    Propagate derivatives through angle(eigenvalues(C)).

--- tests/test_grad_dlambda_dC.py
+++ tests/test_grad_dlambda_dC.py
@@ -2,19 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def lambda_func(C):
-    lambs, us = np.linalg.eig(C)
-    return lambs[0]
+# Config.
+np.random.seed(111)
+c_0 = 0.9
+w = 0.1
 
 
-def dlambda_dC_total(C):
-    lambs, us = np.linalg.eig(C)
+def theta_func(d):
+    C = np.array(
+        [[0., -np.exp(2 * 1j * w)],
+         [1., np.exp(1j * w) / (c_0 * d) * (c_0**2 + d**2 - 1.)]])
+    eig_vals, eig_vectors = np.linalg.eig(C)
+    eig_idx = np.argmax(eig_vals)
+    eig_val = eig_vals[eig_idx]
+    eig_vector = eig_vectors[eig_idx]
 
-    dC_dd = np.array([[0., 1.],
-                      [0., 0.]])
+    # return eig_val
+    # return np.log(eig_val) * (0. - 1.j)
+    return np.angle(eig_val)
 
-    print(np.matmul(us.conj().T, np.matmul(dC_dd, us)))
-    return np.matmul(us.conj().T, np.matmul(dC_dd, us))[0]
+
+def dtheta_dd_total(d):
+    C = np.array(
+        [[0., -np.exp(2 * 1j * w)],
+         [1., np.exp(1j * w) / (c_0 * d) * (c_0**2 + d**2 - 1.)]])
+    eig_vals, eig_vectors = np.linalg.eig(C)
+    eig_idx = np.argmax(eig_vals)
+    eig_val = eig_vals[eig_idx]
+    eig_vector = eig_vectors[eig_idx]
+
+    dC_dd_total = np.array(
+        [[0., 0.],
+         [0., np.exp(1j * w) * (d**2 - c_0**2 + 1) / (d**2 * c_0)]])
+    dlambda_dd_total = np.diag(np.linalg.inv(eig_vectors) @ dC_dd_total @ eig_vectors)[eig_idx]
+
+    # return dlambda_dd_total
+    print(eig_val, np.abs(eig_val), dlambda_dd_total, -1j / eig_val * dlambda_dd_total)
+    return np.real(-1j / eig_val * dlambda_dd_total)
 
 
 def grad_arrow(x_draw, x, y, grad):
@@ -23,29 +47,26 @@ def grad_arrow(x_draw, x, y, grad):
 
 
 while True:
-    d_a = 8.
-    C_a = np.array([[1., -3. + d_a],
-                    [-6. + 1.j, 9.]])
-    lamb_a = lambda_func(C_a)
+    d_a = np.random.uniform(0.85, 1.15)
+    theta_a = theta_func(d_a)
 
-    epsilon = 1e-6
+    epsilon = 1.e-6
     d_b = d_a + epsilon
-    C_b = np.array([[1., -3. + d_b],
-                    [-6. + 1.j, 9.]])
-    lamb_b = lambda_func(C_b)
+    theta_b = theta_func(d_b)
 
-    lamb_a_grad = dlambda_dC_total(C_a)
+    theta_a_grad = dtheta_dd_total(d_a)
 
-    plt.scatter(d_a, np.real(lamb_a),
-                label='$e_1$')
-    plt.scatter(d_b, np.real(lamb_b),
-                label='$e_1 + \\epsilon$: $\\epsilon={}$'.format(epsilon))
+    plt.scatter(d_a, theta_a,
+                label='$d$')
+    plt.scatter(d_b, theta_b,
+                label='$d + \\epsilon$: $\\epsilon={}$'.format(epsilon))
     x_arrow = np.linspace(d_a, d_b, 2)
-    plt.plot(x_arrow, grad_arrow(x_arrow, d_a, np.real(lamb_a), np.real(lamb_a_grad)),
-             label='Gradient: $\\frac{d \sin{f}}{de}$')
+    plt.plot(x_arrow, grad_arrow(
+        x_arrow, d_a, theta_a, theta_a_grad),
+        label='Gradient: $\\frac{d \\theta_1}{dd}$')
 
     plt.legend()
-    plt.xlabel('e')
-    plt.ylabel('$\sin{f}$')
+    plt.xlabel('d')
+    plt.ylabel('$\\theta_1$')
     plt.tight_layout()
     plt.show()
--- tests/test_jax_derivatives.py
+++ tests/test_jax_derivatives.py
@@ -1,6 +1,6 @@
-import jax
 import numpy as np
 from jax import grad
+import jax.numpy as jnp
 from jax.config import config
 import matplotlib.pyplot as plt
 
@@ -8,25 +8,30 @@ import matplotlib.pyplot as plt
 # Config.
 config.update("jax_enable_x64", True)
 np.random.seed(111)
+c_0 = 0.2
+w = 0.5
 
 
-def lambda_func(d):
-    c_0 = 0.2
-    w = 0.1
-    C = jax.numpy.array(
+def theta_func(d):
+    C = jnp.array(
         [[0., -np.exp(2 * 1j * w)],
          [1., np.exp(1j * w) / (c_0 * d) * (c_0**2 + d**2 - 1.**2)]])
-    eigs = jax.numpy.linalg.eigvals(C)
+    eigs = jnp.linalg.eigvals(C)
 
     # return w - jax.numpy.arccos((c_0**2 + d**2 - 1**2) / (2 * c_0 * d))
-    # return jax.numpy.real(jax.numpy.log(eigs[0]) * (0. + 1.j))
-    return jax.numpy.angle(eigs[0])
+    # return jax.numpy.log(eigs[0]) * (0. - 1.j)
+    # NB. just for this test: select max eigenvalue to ensure we
+    # get same eigenvalue between calls of this function, independent
+    # of the index position jnp.linalg.eigvals orders.
+    eig = jnp.max(eigs)
+    # print(eig, jnp.abs(eig), jnp.angle(eig))
+    return jnp.angle(eig)
 
 
-def dlambda_dC_total(d):
-    eig_grad = grad(lambda_func, holomorphic=False)
+def dtheta_dd_total(d):
+    eig_grad = grad(theta_func, holomorphic=False)
 
-    return jax.numpy.real(eig_grad(d))
+    return eig_grad(d)
 
 
 def grad_arrow(x_draw, x, y, grad):
@@ -35,30 +40,28 @@ def grad_arrow(x_draw, x, y, grad):
 
 
 while True:
-    # NB. may need to newton polish roots to double
-    # precision for good gradients.
     d_a = np.random.uniform(0.85, 1.15)
-    lamb_a = lambda_func(d_a)
+    theta_a = theta_func(d_a)
 
     epsilon = 1.e-6
     d_b = d_a + epsilon
-    lamb_b = lambda_func(d_b)
+    theta_b = theta_func(d_b)
 
-    lamb_a_grad = dlambda_dC_total(d_a)
-    print(lamb_a_grad)
+    theta_a_grad = dtheta_dd_total(d_a)
+    print(theta_a_grad)
 
-    plt.scatter(d_a, lamb_a,
-                label='$e_1$')
-    plt.scatter(d_b, lamb_b,
-                label='$e_1 + \\epsilon$: $\\epsilon={}$'.format(epsilon))
+    plt.scatter(d_a, theta_a,
+                label='$d$')
+    plt.scatter(d_b, theta_b,
+                label='$d + \\epsilon$: $\\epsilon={}$'.format(epsilon))
     x_arrow = np.linspace(d_a, d_b, 2)
     plt.plot(x_arrow, grad_arrow(
-        x_arrow, d_a, lamb_a, lamb_a_grad),
-        label='Gradient: $\\frac{d \sin{f}}{de}$')
+        x_arrow, d_a, theta_a, theta_a_grad),
+        label='Gradient: $\\frac{d \\theta_1}{dd}$')
 
     plt.legend()
-    plt.xlabel('e')
-    plt.ylabel('$\sin{f}$')
+    plt.xlabel('d')
+    plt.ylabel('$\\theta_1$')
     plt.tight_layout()
     plt.show()
 
