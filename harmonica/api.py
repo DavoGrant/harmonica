511fcbed84252652f37d93199916da1c331e8267
David Grant <davidttgg@gmail.com>
2022-05-30 10:26:36 +0100

    Api doc strings update.

--- harmonica/api.py
+++ harmonica/api.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 from harmonica import bindings
 
 
@@ -12,10 +11,21 @@ class HarmonicaTransit(object):
 
     Parameters
     ----------
-    times : type
-        Description [units].
-    ds : type
-        Description [units].
+    times : ndarray, optional
+        1D array of model evaluation times [days].
+    ds : ndarray, optional
+        1D array of planet-star separations [stellar radii].
+    nus : ndarray, optional
+        1D array of planet-star angles [radians].
+    require_gradients : bool, optional
+        Compute derivatives of model with respect to input parameters.
+        This mode should be enabled when using a gradient-based
+        inference methods, such Hamiltonian Monte Carlo.
+    pnl_c and pnl_e : int, optional
+        Number of legendre roots used to approximate the integrals
+        with no closed form solution. pnl_c corresponds to when the
+        planet lies entirely inside the stellar disc, and pnl_e
+        corresponds to when the planet intersects the stellar limb.
 
     Methods
     -------
@@ -29,6 +39,9 @@ class HarmonicaTransit(object):
     Notes
     -----
     Some notes about where the method is described.
+
+    For light curve modelling, either times or ds and nus should be
+    provided.
 
     Perhaps a further note about the limb darkening for common uses.
     For example a quadratic law; u=[0.1, 0.2], limb_dark_law=`integers`,
@@ -104,11 +117,10 @@ class HarmonicaTransit(object):
             Semi-major axis [stellar radii].
         inc : float
             Orbital inclination [radians].
-        ecc : float
-            Eccentricity [], 0 <= ecc < 1.
-        omega : float
-            Argument of periastron [radians]. If ecc is not 0.
-            then omega must also be specified.
+        ecc : float, optional
+            Eccentricity [], 0 <= ecc < 1. Default=0.
+        omega : float, optional
+            Argument of periastron [radians]. Default=0.
 
         """
         self._t0 = t0
@@ -125,12 +137,13 @@ class HarmonicaTransit(object):
 
         Parameters
         ----------
-        limb_dark_law : string; `quadratic` or `non-linear`
-            The stellar limb darkening law.
-        u :  (N,) array_like
-            Limb-darkening coefficients. 1D array of coefficients that
-            correspond to the limb darkening law specified by the
-            limb_dark_law.
+        u : ndarray,
+            1D array of limb-darkening coefficients which correspond to
+            the limb-darkening law specified by limb_dark_law. The
+            quadratic law requires two coefficients and the non-linear
+            law requires four coefficients.
+        limb_dark_law : string, optional; `quadratic` or `non-linear`
+            The stellar limb darkening law. Default=`quadratic`.
 
         """
         self._u = u
@@ -145,16 +158,18 @@ class HarmonicaTransit(object):
 
         Parameters
         ----------
-        r :  (N,) or (N, M) array_like
+        r : ndarray (N,) or (N, M)
             Transmission string coefficients. 1D array of N Fourier
             coefficients that specify the planet radius as a function
-            of angle in the sky-plane. Coefficients correspond to
+            of angle in the sky-plane.
+
             ``r_{\rm{p}}(\theta) = \sum_{n=0}^N a_n \cos{(n \theta)}
-            + \sum_{n=1}^N b_n \csin{(n \theta)}`` where the resulting
-            input is r=[a_0, a_1, b_1, a_2, b_2,..]. For time-dependent
-            transmission strings, use a 2D array with N Fourier
-            coefficients and M time steps, where M is equal to the
-            number of model evaluation epochs.
+            + \sum_{n=1}^N b_n \csin{(n \theta)}``
+
+            The input array is given as r=[a_0, a_1, b_1, a_2, b_2,..].
+            For time-dependent transmission strings, use a 2D array
+            with N Fourier coefficients and M time steps, where M is
+            equal to the number of model evaluation epochs.
 
         """
         self._r = r
@@ -193,36 +208,44 @@ class HarmonicaTransit(object):
 
     def get_planet_transmission_string(self, theta):
         """
-        Get transmission string evaluated at given angles.
+        Get transmission string evaluated at an array of angles.
 
         Parameters
         ----------
-        name : type
-            Description of parameter.
+        theta : ndarray,
+            1D array of angles at which to evaluate the transmission
+            string.
 
         Returns
         -------
-        name : type
-            Description of return object.
+        r_p : ndarray,
+            The transmission string, ``r_{\rm{p}}(\theta)``, evaluated
+            at the provided thetas.
 
         """
         transmission_string = np.empty(theta.shape, dtype=np.float64, order='C')
         bindings.transmission_string(self._r, theta, transmission_string)
         return transmission_string
 
-    def get_precision_estimate(self, N_l):
+    def get_precision_estimate(self):
         """
-        Get light curve precision estimates.
-
-        Parameters
-        ----------
-        name : type
-            Description of parameter.
+        Get light curve precision estimate.
 
         Returns
         -------
-        name : type
-            Description of return object.
+        residuals : ndarray
+            Difference between light curve generated at user set precision
+            and the light curve at max precision.
 
         """
-        return
+        # Get light curve for user set precision.
+        lc_user = self.get_transit_light_curve()
+
+        # Get light curve at max precision.
+        lc_best = np.empty(lc_user.shape, dtype=np.float64, order='C')
+        bindings.light_curve(self._ld_mode, self._u, self._r,
+                             self.ds, self.nus, lc_best,
+                             self.ds_grad, self.nus_grad, self.lc_grad,
+                             500, 500, False)
+
+        return lc_user - lc_best
--- tests/demo/3_extensions/3.1_more_model_options.py
+++ tests/demo/3_extensions/3.1_more_model_options.py
@@ -1,2 +1,3 @@
 # Extension: time-dependent transmission strings.
 # Extension: model precision and runtime tradeoff.
+# Extension: custom orbit (hint circumbinary or asteroid) provided.
